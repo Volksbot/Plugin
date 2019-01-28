09cd610510f46a311f5ae7b0b525970762cc9048
Volksbot <47117471+Volksbot@users.noreply.github.com>
2019-01-28 18:17:14 +0100

    Add files via upload

---
+++ info/src/Controller/InfoController.php
@@ -0,0 +1,39 @@
+<?php
+namespace Drupal\info\Controller;
+class InfoController {
+  public function info() {
+    return array(
+      //Einleitung
+      '#title' => 'Bot@HWR',
+      '#markup' =>'Bot@HWR ist ein fachbereichsübergreifendes Vorhaben mit dem Ziel 
+      den Forschungsschwerpunkt Robotik an der Hochschule für Wirtschaft und Recht
+      Berlin auszubauen. Es sollen anwendungsorientierte und interdisziplinäre
+      Forschungsprojekte initiiert werden um insbesondere neue Robotik-Anwendungsfelde
+      zu ergründen. Im Fokus steht dabei zunächst der Sektor Pflege und Gesundheit.
+      Zum Projektbeginn stehen für das Erstellen von Prototypen zwei modulare 
+      Roboterbaukastensysteme (Volksbot) des Fraunhofer IAIS zur Verfügung. 
+      Die fachlichen Schwerpunkte des interdisziplinären Teams liegen in der Robotik,
+      Diversity-Strategien und in Partizipativer Softwareentwicklung. <br> <p></p>
+      <b>Zuwendungsempfänger:</b>	Hochschule für Wirtschaft und Recht Berlin <br>
+     <b>Fördergeber:</b>	Institut für angewandte Forschung Berlin e.V. <br>
+     <b>Zeitraum:</b>	seit 01.10.2016 <br>
+     <b>Beteiligte Personen:</b>	 <br>
+     <b>Prof. Dr. Heike Wiesner (Projektleitung) </b><br>
+     Prof. Dr. Dagmar Monett Diaz<br>
+     Holger Zimmermann<br>
+     Hermann Stolte<br>
+     <b>Verwandte Projekte</b>	<br>
+     MINT4HWR<br>
+     Roberta<br>
+     Partizipative Gestaltung technologieunterstützter Lernszenarien<br>
+     <b>Weiterführende Links:</b> <br>
+      https://botathwr.github.io/ <br>
+      http://www.mint4.de/ <br>
+      https://www.hwr-berlin.de/
+      '
+
+
+    );
+
+  }
+}
